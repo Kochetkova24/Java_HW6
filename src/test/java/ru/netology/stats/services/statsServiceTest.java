faa0ad003528957f13fcceeafe76a20fcbd2af1f
Kochetkova Tanya <kta240190@gmail.com>
2024-02-25 23:00:04 +0500

    renamaed service

--- src/test/java/ru/netology/stats/services/statsServiceTest.java
+++ src/test/java/ru/netology/stats/services/statsServiceTest.java
@@ -3,7 +3,7 @@ package ru.netology.stats.services;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class StatsServiceTest {
+public class statsServiceTest {
 
     @Test
     public void SumSalesTest() {
